21d0e06b8f65451a57af5406dca92c9c16a2ca26
Tiphani-Campbell <tiphani.campbell@gmail.com>
2022-04-02 19:49:37 -0500

    Added search bar

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,4 +1,12 @@
 <template>
+    <form  @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
+        <div class = "input-group mx-sm-3 mb-2">
+            <label class="visually-hidden" for="search">Search</label>
+            <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here"/>
+            <button class="btn btn-primary mb-2">Search</button>
+        </div>
+        <p>You are searching for {{ searchTerm }}</p>
+    </form>
     <ul class="news__list">
         <li v-for="article in articles" class="news__item">  <br> <img :src="article.urlToImage" class="article-image"/> <br> <h4> {{ article.title }} </h4> <br> <p> {{ article.description }}</p></li>
     </ul>
@@ -8,7 +16,26 @@
 export default{
     data() {
         return {
-            articles: []
+            articles: [],
+            searchTerm: ''
+        }
+    },
+    methods:{
+        searchNews(){
+            let self =this;
+
+            fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + 'language=en', {
+                headers: {
+                    'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`,
+                }
+            })
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                console.log(data);
+                self.articles = data.articles;
+            });
         }
     },
     created(){
